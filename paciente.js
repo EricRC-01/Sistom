76093eba1d96f365d99b8260c4c16995336f2d69
Eduarda Tuboy <eduardatuboy@gmail.com>
2024-03-11 17:21:37 -0300

    Paginação completa!

---
+++ paciente.js
@@ -0,0 +1,18 @@
+function openSection(evt, sectionName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("botoes-paginacao");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sectionName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+// Chame esta função para abrir a primeira tab por padrão ao carregar a página
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".tablinks").click();
+});

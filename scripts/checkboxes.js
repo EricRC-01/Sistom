d2bac83f9cc9ddb558eacfd2234be8b838ad8907
Eduarda Tuboy <eduardatuboy@gmail.com>
2024-03-23 16:27:21 -0300

    atualização das checkboxes

---
+++ scripts/checkboxes.js
@@ -0,0 +1,14 @@
+
+function texto(id, novoTexto) {
+  // Encontra o elemento input pelo seu ID
+  var checkbox = document.getElementById(id);
+
+  // Encontra o elemento label que está imediatamente após o input
+  var label = checkbox.nextElementSibling;
+
+  // Verifica se o label existe e tem o atributo for correspondente ao id do input
+  if (label && label.tagName === 'LABEL' && label.getAttribute('for') === id) {
+    // Define o novo texto usando o atributo data-content, mas somente altera o visual quando marcado
+    label.setAttribute('data-content', novoTexto);
+  }
+}

b02c6b8d97496b867b4a25782b0ee385e1cf8798
Gustavo <gusferreira1203@usp.br>
2024-04-10 00:31:04 -0300

    cores variabilizadas

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,13 @@
 function toggleMenu(toggleBtn) {
+  var icon = toggleBtn.querySelector("ion-icon");
   var menuContent = toggleBtn.nextElementSibling;
+
+  if (icon.name === "caret-down-outline") {
+    icon.name = "caret-up-outline"; // Troca para a seta para cima
+  } else {
+    icon.name = "caret-down-outline"; // Troca para a seta para baixo
+  }
+
   menuContent.style.display =
     menuContent.style.display === "block" ? "none" : "block";
 }
